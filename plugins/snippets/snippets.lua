77dbd5f10be82a0a9c446d05dba014b05127e55e
Hans Peyrot <hans@eflow.com.mx>
2019-09-16 21:57:46 -0500

    update

--- plugins/snippets/snippets.lua
+++ plugins/snippets/snippets.lua
@@ -11,122 +11,6 @@ Location.__index = Location
 local Snippet = {}
 Snippet.__index = Snippet
 
--- returns the number of bytes used by the UTF-8 character at byte i in s
--- also doubles as a UTF-8 character validator
-function utf8charbytes (s, i)
-    -- argument defaults
-    i = i or 1
-    local c = string.byte(s, i)
-
-    -- determine bytes needed for character, based on RFC 3629
-    if c > 0 and c <= 127 then
-        -- UTF8-1
-        return 1
-    elseif c >= 194 and c <= 223 then
-        -- UTF8-2
-        local c2 = string.byte(s, i + 1)
-        return 2
-    elseif c >= 224 and c <= 239 then
-        -- UTF8-3
-        local c2 = s:byte(i + 1)
-        local c3 = s:byte(i + 2)
-        return 3
-    elseif c >= 240 and c <= 244 then
-        -- UTF8-4
-        local c2 = s:byte(i + 1)
-        local c3 = s:byte(i + 2)
-        local c4 = s:byte(i + 3)
-        return 4
-    end
-end
-
--- returns the number of characters in a UTF-8 string
-function utf8len (s)
-    local pos = 1
-    local bytes = string.len(s)
-    local len = 0
-
-    while pos <= bytes and len ~= chars do
-        local c = string.byte(s,pos)
-        len = len + 1
-
-        pos = pos + utf8charbytes(s, pos)
-    end
-
-    if chars ~= nil then
-        return pos - 1
-    end
-
-    return len
-end
-
--- functions identically to string.sub except that i and j are UTF-8 characters
--- instead of bytes
-function utf8sub (s, i, j)
-    j = j or -1
-
-    if i == nil then
-        return ""
-    end
-
-    local pos = 1
-    local bytes = string.len(s)
-    local len = 0
-
-    -- only set l if i or j is negative
-    local l = (i >= 0 and j >= 0) or utf8len(s)
-    local startChar = (i >= 0) and i or l + i + 1
-    local endChar = (j >= 0) and j or l + j + 1
-
-
-    -- Want's nothing
-    if startChar == endChar then
-        return ""
-    end
-    
-    -- can't have start before end!
-    if startChar > endChar then
-        return ""
-    end
-
-    -- byte offsets to pass to string.sub
-    local startByte, endByte = 1, bytes
-
-    while pos <= bytes do
-        len = len + 1
-
-        if len == startChar then
-            startByte = pos
-        end
-
-        pos = pos + utf8charbytes(s, pos)
-
-        if len == endChar then
-            endByte = pos - 1
-            break
-        end
-    end
-
-    return string.sub(s, startByte, endByte)
-end
-
--- replace UTF-8 characters based on a mapping table
-function utf8replace (s, mapping)
-    local pos = 1
-    local bytes = string.len(s)
-    local charbytes
-    local newstr = ""
-
-    while pos <= bytes do
-        charbytes = utf8charbytes(s, pos)
-        local c = string.sub(s, pos, pos + charbytes - 1)
-        newstr = newstr .. (mapping[c] or c)
-        pos = pos + charbytes
-    end
-
-    return newstr
-end
-
 function Location.new(idx, ph, snip)
 	local self = setmetatable({}, Location)
 	self.idx = idx
@@ -219,7 +103,11 @@ function Location.handleInput(self, ev)
 			if v == nil then
 				v = ""
 			end
-			self.ph.value = utf8sub(v,0, offset-1) .. ev.Deltas[1].Text .. utf8sub(v,offset)
+			if offset==1 then
+				self.ph.value = ev.Deltas[1].Text .. utf8sub(v,offset)
+			else
+				self.ph.value = utf8sub(v,0, offset-1) .. ev.Deltas[1].Text .. utf8sub(v,offset)
+			end
 			self.snippet:insert()
 			return true
 		end
@@ -244,7 +132,11 @@ function Location.handleInput(self, ev)
 		end
 
 		local len = ev.Deltas[1].End.X - ev.Deltas[1].Start.X
-		self.ph.value = utf8sub(v,0, offset-1) .. utf8sub(v,offset+len)
+		if offset==1 then
+			self.ph.value = utf8sub(v,offset+len)
+		else
+			self.ph.value = utf8sub(v,0, offset-1) .. utf8sub(v,offset+len)
+		end
 		self.snippet:insert()
 		return true
 	end
