258f7affddef324a5280ca6d27a6215d485c5d80
Hans Peyrot <hans@eflow.com.mx>
2025-01-13 15:22:03 -0600

    update plugins

--- plugins/go/go.lua
+++ plugins/go/go.lua
@@ -1,53 +1,53 @@
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 local ErrorView = nil
 local writesettings = false
 
-if GetPluginOption("go","version") == nil then
-	AddPluginOption("go","version", VERSION)
-	writesettings = true
-elseif GetPluginOption("go","version") ~= VERSION then
-	SetPluginOption("go","version", VERSION)
-	writesettings = true
+if GetPluginOption("go", "version") == nil then
+    AddPluginOption("go", "version", VERSION)
+    writesettings = true
+elseif GetPluginOption("go", "version") ~= VERSION then
+    SetPluginOption("go", "version", VERSION)
+    writesettings = true
 end
 
-if GetPluginOption("go","goimports") == nil then
-    AddPluginOption("go","goimports", false)
-	writesettings = true
+if GetPluginOption("go", "goimports") == nil then
+    AddPluginOption("go", "goimports", false)
+    writesettings = true
 end
 
-if GetPluginOption("go","gofmt") == nil then
-    AddPluginOption("go","gofmt", true)
-	writesettings = true
+if GetPluginOption("go", "gofmt") == nil then
+    AddPluginOption("go", "gofmt", true)
+    writesettings = true
 end
 
 if writesettings then
-	WritePluginSettings("go")
+    WritePluginSettings("go")
 end
 
-MakeCommand("goimports", "go.goimports", 0)
-MakeCommand("gofmt", "go.gofmt", 0)
+--MakeCommand("goimports", "go.goimports", 0)
+--MakeCommand("gofmt", "go.gofmt", 0)
 AddRuntimeFile("go", "help", "help/go-plugin.md")
 
 function onSave(view)
-    if GetPluginOption("go","goimports") then
+    if GetPluginOption("go", "goimports") then
         goimports()
     end
-    if GetPluginOption("go","gofmt") then
+    if GetPluginOption("go", "gofmt") then
         return gofmt(view)
     end
 end
 
 function gofmt(view)
     local ps = 0
-    
-	msg,err=ExecCommand("gofmt","-e",CurView().Buf.Path)
+
+    msg, err = ExecCommand("gofmt", "-e", CurView().Buf.Path)
     if err ~= nil then
         if ErrorView == nil then
             view:HSplitIndex(NewBuffer(msg, "Error"), 1)
             ErrorView = CurView()
-            ErrorView.Type.Kind=2
+            ErrorView.Type.Kind = 2
             ErrorView.Type.Readonly = true
             ErrorView.Type.Scratch = true
             SetLocalOption("softwrap", "true", ErrorView)
@@ -64,39 +64,39 @@ function gofmt(view)
         else
             local pos = ErrorView.Buf:Start()
             ErrorView.Buf:deleteToEnd(pos)
-            ErrorView.Buf:insert(pos,msg)
+            ErrorView.Buf:insert(pos, msg)
         end
-		local xy={}
-		xy.X = 0
-		xy.Y = -99
-		for ch in string.gmatch(msg,"(%d+):") do
-			xy.Y = tonumber(ch)-1;
-			break
-		end
-		if xy.Y ~= -99  then
-			if xy.Y<0 then
-				xy.Y=0
-			end
-			if ps==1 then
-			    view:PreviousSplit(false)
-		    end
-			view.Cursor:GotoLoc(xy)
-		end
-		messenger:Error("Syntax Error")
-		return false
+        local xy={}
+        xy.X = 0
+        xy.Y = -99
+        for ch in string.gmatch(msg, "(%d+):") do
+            xy.Y = tonumber(ch)-1;
+            break
+        end
+        if xy.Y ~= -99  then
+            if xy.Y < 0 then
+                xy.Y = 0
+            end
+            if ps == 1 then
+                view:PreviousSplit(false)
+            end
+            view.Cursor:GotoLoc(xy)
+        end
+        messenger:Error("Syntax Error")
+        return false
     else
-	    if ErrorView ~= nil then
-	        ErrorView:Quit(false)
-	    end
+        if ErrorView ~= nil then
+            ErrorView:Quit(false)
+        end
         ErrorView = nil
         CurView():Save(false)
         local handle = io.popen("gofmt -w " .. CurView().Buf.Path)
         local result = handle:read("*a")
         handle:close()
         CurView():ReOpen()
-	    messenger:Success("Syntax OK")
-	    return true
-	end
+        messenger:Success("Syntax OK")
+        return true
+    end
 end
 
 function goimports()
