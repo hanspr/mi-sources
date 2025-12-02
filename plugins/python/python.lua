d1f052a7ab3a9650dc337ff1f3c9fc7599154c87
Hans Peyrot <hans@eflow.com.mx>
2025-12-01 19:39:34 -0600

    add python plugin

---
+++ plugins/python/python.lua
@@ -0,0 +1,45 @@
+
+VERSION = "1.0.1"
+
+local indent = -1
+local home = os.getenv("HOME")
+
+if GetPluginOption("python", "version") == nil then
+    AddPluginOption("python", "version", VERSION)
+    writesettings = true
+elseif GetPluginOption("python", "version") ~= VERSION then
+    SetPluginOption("python", "version", VERSION)
+    writesettings = true
+end
+
+if writesettings then
+    WritePluginSettings("python")
+end
+
+function preInsertNewline(view)
+    indent = -1
+    view = CurView()
+    xy = {}
+    xy.X = view.Cursor.Loc.X
+    xy.Y = view.Cursor.Loc.Y
+    line = view.Buf:Line(xy.Y)
+    if string.find(line, ":") then
+        indent = #GetLeadingWhitespace(line)
+    end
+end
+
+function onInsertNewline(view)
+    if indent < 0 then
+        return true
+    end
+    xy = {}
+    xy.X = view.Cursor.Loc.X
+    xy.Y = view.Cursor.Loc.Y
+    line = view.Buf:Line(xy.Y)
+    line_indent = #GetLeadingWhitespace(line)
+    if line_indent <= indent then
+        view:InsertTab(false)
+    end
+    return true
+end
+
