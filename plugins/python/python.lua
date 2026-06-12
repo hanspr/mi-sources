de09bf65859aa255d617019f5c58656f4217e4f7
Hans Peyrot <hans@eflow.com.mx>
2026-06-12 17:25:51 -0600

    syntax check python project

--- plugins/python/python.lua
+++ plugins/python/python.lua
@@ -1,8 +1,12 @@
 
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 
 local indent = -1
 local home = os.getenv("HOME")
+local curLoc = {}
+
+curLoc.X = 0
+curLoc.Y = -1
 
 if GetPluginOption("python", "version") == nil then
     AddPluginOption("python", "version", VERSION)
@@ -14,6 +18,58 @@ end
 
 if writesettings then
     WritePluginSettings("python")
+end
+
+function Check(view, fpath)
+    local ps = 0
+    msg, err = ExecCommand("python3", "-m", "py_compile", fpath)
+    if err ~= nil then
+        if view:GetHelperView() == nil then
+            curLoc.X = view.Cursor.Loc.X
+            curLoc.Y = view.Cursor.Loc.Y
+            ps = 1
+        end
+        view:OpenHelperView("h", "", msg)
+        if ps == 1 then
+            view:PreviousSplit(false)
+        end
+        local xy = {}
+        xy.X = 0
+        xy.Y = -99
+        if string.find(msg, "EOF") == nil then
+            for ch in string.gmatch(msg, "line (%d+)") do
+                xy.Y = tonumber(ch)-1;
+                break
+            end
+        end
+        if xy.Y ~= -99  then
+            if xy.Y < 0 then
+                xy.Y = 0
+            end
+            view.Cursor:GotoLoc(xy)
+            view:Center(false)
+            view:Relocate()
+        end
+        messenger:Error("Syntax Error")
+        return false
+    else
+        if view:GetHelperView() ~= nil then
+            view:CloseHelperView()
+        end
+        if curLoc.Y ~= -1 then
+            view:SetLastView()
+            view.Cursor:GotoLoc(curLoc)
+            view:Center(false)
+            view:Relocate()
+        end
+        curLoc.Y = -1
+    end
+end
+
+function onSave(view)
+    local fpath = CurView().Buf.Path
+
+    return Check(view, fpath)
 end
 
 function preInsertNewline(view)
